701d489683fb2c0e75f9e9dbad3426af5fbcc69f
RodrigoMejiaDiaz <rodrigoemdiaz@hotmail.com>
2023-03-27 15:09:47 -0500

    keyfile json

--- routes/api/rutas.js
+++ routes/api/rutas.js
@@ -26,20 +26,18 @@ const fileFilter = (req, file, cb) => {
 
 let upload = multer({ storage: storageM, fileFilter });
 
-const keyfile = [
-  [
-    process.env.type,
-    process.env.project_id,
-    process.env.private_key_id,
-    process.env.private_key,
-    process.env.client_email,
-    process.env.client_id,
-    process.env.auth_uri,
-    process.env.token_uri,
-    process.env.auth_provider_x509_cert_url,
-    process.env.client_x509_cert_url,
-  ],
-];
+const keyfile = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key,
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+};
 
 const storage = new Storage({
   projectId: "deep-clock-381322",
